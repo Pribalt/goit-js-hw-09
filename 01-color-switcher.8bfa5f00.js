ce83b1037c49c7650217dfe9cb0f745e1f73d3e5
Pribalt <99962979+Pribalt@users.noreply.github.com>
2023-01-02 20:01:10 +0000

    Deploying to gh-pages from @ Pribalt/goit-js-hw-09@25aac22a8b3fd89cdbbce7c4c77bc8f5fda361ef 🚀

---
+++ 01-color-switcher.8bfa5f00.js
@@ -0,0 +1,2 @@
+const t=document.querySelector("button[data-start]"),e=document.querySelector("button[data-stop]"),n=document.body;let d=null;t.addEventListener("click",(function(o){d=setInterval((()=>{t.disabled=!0,e.disabled=!1;const d=`#${Math.floor(16777215*Math.random()).toString(16)}`;n.style.backgroundColor=d}),1e3)})),e.addEventListener("click",(function(){e.disabled=!0,t.disabled=!1,clearInterval(d)}));
+//# sourceMappingURL=01-color-switcher.8bfa5f00.js.map
