d53263eef91b6480ffb989dc95409923039ff356
Pribalt <vitalisvitalik@gmail.com>
2023-01-07 13:16:20 +0200

    change1-03-promises.js

--- src/js/03-promises.js
+++ src/js/03-promises.js
@@ -19,12 +19,22 @@ function promiseCycle(delay, step, amount) {
   for (let i = 0; i < amount; i += 1) {
     const positionCycle = i + 1;
     const delayCycle = delay + step * i;
-    createPromise(positionCycle, delayCycle);
+    createPromise(positionCycle, delayCycle)
+      .then(({ position, delay }) => {
+        Notiflix.Notify.success(
+          `✅ Fulfilled promise ${position} in ${delay}ms`
+        );
+      })
+      .catch(({ position, delay }) => {
+        Notiflix.Notify.failure(
+          `❌ Rejected promise ${position} in ${delay}ms`
+        );
+      });
   }
 }
 
 function createPromise(position, delay) {
-  const promise = new Promise((resolve, reject) => {
+  return new Promise((resolve, reject) => {
     const shouldResolve = Math.random() > 0.3;
 
     setTimeout(() => {
@@ -37,11 +47,4 @@ function createPromise(position, delay) {
       }
     }, delay);
   });
-  return promise
-    .then(({ position, delay }) => {
-      Notiflix.Notify.success(`✅ Fulfilled promise ${position} in ${delay}ms`);
-    })
-    .catch(({ position, delay }) => {
-      Notiflix.Notify.failure(`❌ Rejected promise ${position} in ${delay}ms`);
-    });
 }
